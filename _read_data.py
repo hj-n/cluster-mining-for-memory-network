5e8da386c4865f80904b2dd16be01a1213a07a88
Hyeon Jeon <hj@hcil.snu.ac.kr>
2022-08-24 06:05:23 +0900

    modified read_data function to deal with entire datasets{

--- _read_data.py
+++ _read_data.py
@@ -8,6 +8,14 @@ from tqdm import tqdm
 def read_data(file_identifier, window=12, stride=6):
 	if file_identifier == "elec":
 		data = pd.read_csv("data/electricity/electricity.txt", header=None, sep=",")
+	elif file_identifier == "exchange":
+		data = pd.read_csv("data/exchange_rate/exchange_rate.txt", header=None, sep=",")
+	elif file_identifier == "solar":
+		data = pd.read_csv("data/solar_energe/solar_AL.txt", header=None, sep=",")
+	elif file_identifier == "traffic":
+		data = pd.read_csv("data/traffic/traffic.txt", header=None, sep=",")
+	else:
+		raise ValueError("Invalid file_identifier")
 
 
 	data_np = data.to_numpy().T
